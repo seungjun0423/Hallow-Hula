e539c73b6dd80e04cb923f9d5c08da75c13f6b28
seungjun0423 <sibonum@gmail.com>
2022-08-21 22:24:55 +0900

    Add components dir

---
+++ components/NavBar.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+
+export default function NavBar () {
+  const router = useRouter();
+  console.log(router)
+
+  return (
+  <nav>
+    <Link href='/'>Home</Link>
+    <Link href='About'>About</Link>
+  </nav>
+  )
+}
